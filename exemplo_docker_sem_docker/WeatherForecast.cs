e80e52656beb8441078940f886330e62b68b3d81
Diogo <diogo@codigocentral.com.br>
2022-11-29 20:20:16 -0300

    Initial commit

---
+++ exemplo_docker_sem_docker/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace exemplo_docker_sem_docker
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
